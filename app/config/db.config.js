01d314c9646372b4d8ce18decc84b060c20a2f2c
19ce074 <19ce074@charusat.edu.in>
2023-03-13 12:01:21 +0530

    Frontend and Backend integrated Sucessfully

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   HOST: "localhost",
   USER: "postgres",
-  PASSWORD: "123",
+  PASSWORD: "Vivek@3011",
   DB: "testdb",
   dialect: "postgres",
   pool: {
--- server.js
+++ server.js
@@ -1,37 +1,33 @@
 const express = require("express");
-// const bodyParser = require("body-parser"); /* deprecated */
+const bodyParser = require("body-parser");
 const cors = require("cors");
 
+const path = __dirname + '/app/views/';
+
 const app = express();
+
+app.use(express.static(path));
 
 var corsOptions = {
   origin: "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
-
-// parse requests of content-type - application/json
-app.use(express.json());  /* bodyParser.json() is deprecated */
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));   /* bodyParser.urlencoded() is deprecated */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require("./app/models");
-db.sequelize.sync();
-// // drop the table if it already exists
-// db.sequelize.sync({ force: true }).then(() => {
-//   console.log("Drop and re-sync db.");
-// });
 
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+db.sequelize.sync();
+
+app.get('/', function (req,res) {
+  res.sendFile(path + "index.html");
 });
 
-require("./app/routes/turorial.routes")(app);
+require("./app/routes/turorial.routes.js")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
-});
+})
